12e7cca174e87adff6201d69fa317ad5562aa47e
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2024-01-09 22:23:44 -0500

    Agregando Docker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const nombre = "Fernando";
+const apellido = "Canales";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let valorDado = 5;
+valorDado = 4;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  let valorDado = 6;
+  console.log(valorDado);
+}
